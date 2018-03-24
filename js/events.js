061fa638a44f8f6127698d4ba4551df09419fb2d
Sean Febo <seanfebo@techie.com>
2018-03-24 21:28:46 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,34 @@
 //define functions here
+function getIt() {
+  $("p").on("click")
+  alert ("Hey!")
+  return;
+}
 
+function frameIt() {
+  $("img").on("load")
+  $("img").addClass("tasty")
+}
+
+function pressIt() {
+  $("form").on("keydown",
+  function(key) {
+  if (key.which === 71)
+  alert ("g was pressed.")
+  }
+  )
+}
+
+function submitIt() {
+  $("form").on("submit")
+  alert ("Your form is going to be submitted now.")
+  return;
+}
 $(document).ready(function(){
 
 // call functions here
-
+getIt()
+frameIt()
+pressIt()
+sumbitIt()
 });
