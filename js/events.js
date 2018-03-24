b1d357e13031176b58b4c017800db2446a6c6871
Sean Febo <seanfebo@techie.com>
2018-03-24 21:18:22 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,5 +1,20 @@
 //define functions here
+function getIt() {
+  $("p").on("click")
+  alert ("Hey!")
+  return;
+}
 
+function frameIt() {
+  $("img").on("load")
+  $("img").addClass("tasty")
+}
+
+function pressIt() {
+  $("form").on("keydown")
+  if (key.which === 071)
+  
+}
 $(document).ready(function(){
 
 // call functions here
